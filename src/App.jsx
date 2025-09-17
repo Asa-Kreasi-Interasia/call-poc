8f956531d771f783b5c760a41909f9dbd013c5c8
mikaelalvianrizky <mikael.alvian.rizky@gmail.com>
2025-09-17 10:57:40 +0700

    c

--- src/App.jsx
+++ src/App.jsx
@@ -42,8 +42,8 @@ function App() {
         <video ref={remoteView} autoPlay></video>
       </div>
       <p>
-        <button onClick={() => call('085155362326')}>Test</button>
-        <button onClick={() => call('9085155362326')}>Test</button>
+        <button onClick={() => call('082128525299')}>Test</button>
+        <button onClick={() => call('9082128525299')}>Test</button>
       </p>
     </div>
   );
